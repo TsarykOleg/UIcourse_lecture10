1ca2e51013dbc78a976f2fb849da26ad78884a6d
TsarykOleg <tsaryk.oleg@gmail.com>
2021-10-15 01:11:40 +0300

    added index.html, index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+let myName = 'Tsaryk Oleg';
+let myAge = '44';
+let myAnswer = 'Yes!';
+let message = 'My name is ' + myName + '. I am ' + myAge + ' years.' + ' Do you like learning Front End?-' + myAnswer;
+// console.log(message);
+let button = document.getElementById('btn');
+function example() {
+alert(message)    
+}
+btn.onclick = example;
