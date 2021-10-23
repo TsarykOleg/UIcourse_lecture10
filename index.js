d93151e4881936e795c5633f2cfa0102e174e0bb
TsarykOleg <tsaryk.oleg@gmail.com>
2021-10-23 22:04:57 +0300

    modified index.js

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ let myName = 'Tsaryk Oleg';
 let myAge = '44';
 let myAnswer = 'Yes!';
 let message = 'My name is ' + myName + '. I am ' + myAge + ' years.' + ' Do you like learning Front End?-' + myAnswer;
-// console.log(message);
-let button = document.getElementById('btn');
-function example() {
-alert(message)    
-}
-btn.onclick = example;
+console.log(message)
+btn.onclick = function() {
+    alert(message) 
+};
+
+// let button = document.getElementById('btn');
+// function example() {
+// alert(message)    
+// }
+// btn.onclick = example;
+
+ 
